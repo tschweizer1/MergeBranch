d095361e9257c8f969dd40267c92e51835d62e00
Tyler Schweizer <tschweizer@chapman.edu>
2026-04-21 15:37:02 -0700

    added octo

---
+++ MergeBranch/BlueRingedOctopus.cs
@@ -0,0 +1,9 @@
+﻿namespace GitPractice;
+
+public class BlueRingedOctopus : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("The blue-ringed octopus doesn't have to say anything");
+    }
+}
