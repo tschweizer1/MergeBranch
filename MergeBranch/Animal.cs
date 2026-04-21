8fdafb53d932dc53d2733b1776d3e2a0687a180e
Tyler Schweizer <tschweizer@chapman.edu>
2026-04-21 14:56:31 -0700

    Initial commit

---
+++ MergeBranch/Animal.cs
@@ -0,0 +1,9 @@
+﻿namespace GitPractice;
+
+public class Animal
+{
+    public virtual void MakeSound()
+    {
+        Console.WriteLine("Generic Animal Sound!");
+    }
+}
---
+++ MergeBranch/Main.cs
@@ -0,0 +1,19 @@
+﻿namespace GitPractice;
+
+class MergeBranch
+{
+    static void Main(string[] args)
+    {
+        List<Animal> animals = new List<Animal>();
+        
+        animals.Add(new Cassowary());
+        animals.Add(new BulletAnt());
+        animals.Add(new Chimpanzee());
+        animals.Add(new BlueRingedOctopus());
+        
+        foreach (Animal animal in animals)
+        {
+            animal.MakeSound();
+        }
+    }
+}
