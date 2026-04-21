3161459ec1d4137d6ad2fc0104149b802de1c8a7
a-zagski <zebrazag222@gmail.com>
2026-04-21 15:26:04 -0700

    added cassowary

---
+++ MergeBranch/Cassowary.cs
@@ -0,0 +1,9 @@
+﻿namespace GitPractice;
+
+public class Cassowary : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("The cassowary says: I look like a deranged prehistoric turkey with a helmet so who cares what I sound like");
+    }
+}
